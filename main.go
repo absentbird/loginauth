175b8ae70e0f1d450003c2d5187b96aa966078c2
Shawn Bird <sbird@cfgreens.com>
2022-06-08 14:34:31 -0700

    works great

--- main.go
+++ main.go
@@ -1,31 +1,32 @@
 package loginauth
 
 import (
-    "errors"
-    "net/smtp"
+	"errors"
+	"net/smtp"
 )
 
 type login_auth struct {
-    username string
-    password string
+	username string
+	password string
 }
+
 func (s *login_auth) Start(server *smtp.ServerInfo) (string, []byte, error) {
-    return "LOGIN", []byte(s.username), nil
+	return "LOGIN", []byte(s.username), nil
 }
 func (s *login_auth) Next(fromServer []byte, more bool) ([]byte, error) {
-    if more {
-        switch string(fromServer) {
-        case "Username:":
-            return []byte(s.username), nil
-        case "Password:":
-            return []byte(s.password), nil
-        default:
-            return nil, errors.New("Unknown from server")
-        }
-    }
-    return nil, nil
+	if more {
+		switch string(fromServer) {
+		case "Username:":
+			return []byte(s.username), nil
+		case "Password:":
+			return []byte(s.password), nil
+		default:
+			return nil, errors.New("Unknown from server")
+		}
+	}
+	return nil, nil
 }
 
 func Auth(username, password string) smtp.Auth {
-    return &login_auth{username, password}
+	return &login_auth{username, password}
 }
